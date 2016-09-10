a55634b192154912be5ebe6ac4ca2d5d5dac436c
Devin Watson <devinwatson015@gmail.com>
2016-09-11 04:16:07 +1000

    style changes have been connected with scope

--- js/app.js
+++ js/app.js
@@ -1,41 +1,71 @@
 var myApp = angular.module("myApp", []);
 
-myApp.controller("ButtonCtrl", function ButtonCtrl($scope) {
-  $scope.name ="ENABLE";
-  
-  $scope.enableIt = function(){
+myApp.controller("ButtonCtrl", function ButtonCtrl($scope) {
+    $scope.name = "ENABLE";
 
-    if($scope.name === "ENABLE"){
-      $scope.name = "ENABLED";
+    $scope.enableIt = function() {
 
-      document.getElementById('btn').style = 'left: 130px; -webkit-transition: 1s;transition: 1s; color: black; background-color: white;';
-      document.getElementById('slider').style = '-webkit-transition: 1s;transition: 1s; background-color: #00bcd6; border: 6px solid #00bcd6;';
-      document.getElementById('addons').style = '-webkit-transition: 1s;transition: 1s; background-color: #00c6e2;';
-      document.getElementById('addon-description').style = '-webkit-transition: 1s;transition: 1s; color: #ffffff;';
-      document.getElementById('addon-head').style = '-webkit-transition: 1s;transition: 1s; color: #ffffff;';
-      document.getElementById('checkMark').style = '-webkit-transition: 1s;transition: 1s; color: #ffffff; border: 1px solid #fff;';
-    } else {
-      $scope.name = "ENABLE";
-      document.getElementById('btn').style = 'right: 130px; -webkit-transition: 1s;transition: 1s; color: white; background-color: #00c6e2;';
-      document.getElementById('slider').style = '-webkit-transition: 1s;transition: 1s; background-color: #f9fbfc; border: 6px solid #f9fbfc;';
-      document.getElementById('addons').style = '-webkit-transition: 1s;transition: 1s; background-color: #ffffff';
-      document.getElementById('addon-description').style = '-webkit-transition: 1s;transition: 1s; color: #a4a4a4';
-      document.getElementById('addon-head').style = '-webkit-transition: 1s;transition: 1s; color: #2e313b';    
-      document.getElementById('checkMark').style = '-webkit-transition: 1s;transition: 1s; color: #00c1ff; border: 1px solid #00c1ff;';
-    }
-  };
+        if ($scope.name === "ENABLE") {
+            $scope.name = "ENABLED";
+            $scope.btnStyle = {
+                "left": "130px",
+                "color": "black",
+                "background-color": "white"
+            };
+            $scope.sliderStyle = {
+                "border": "6px solid #00bcd6",
+                "background-color": "#00bcd6"
+            };
+            $scope.addonsStyle = {
+                "background-color": "#00c6e2"
+            };
+            $scope.addonDesStyle = {
+                "color": "#ffffff"
+            };
+            $scope.addonHeadStyle = {
+                "color": "#ffffff"
+            };
+            $scope.checkMarkStyle = {
+                "color": "#ffffff",
+                "border": "1px solid #ffffff"
+            };
+        } else {
+            $scope.name = "ENABLE";
+            $scope.btnStyle = {
+                "right": "130px",
+                "color": "white",
+                "background-color": "#00c6e2"
+            };
+            $scope.sliderStyle = {
+                "border": "6px solid #f9fbfc",
+                "background-color": "#f9fbfc"
+            };
+            $scope.addonsStyle = {
+                "background-color": "#ffffff"
+            };
+            $scope.addonDesStyle = {
+                "color": "#a4a4a4"
+            };
+            $scope.addonHeadStyle = {
+                "color": "#2e313b"
+            };
+            $scope.checkMarkStyle = {
+                "color": "#00c1ff",
+                "border": "1px solid #00c1ff"
+            };
+        }
+    };
 });
 
 myApp.controller("CounterCtrl", function CounterCtrl($scope) {
-  var incremented = false;
-  $scope.count = 120;
+    var incremented = false;
+    $scope.count = 120;
 
-  $scope.increment = function() {
-    if (incremented) { 
-      return; 
-    }
-    $scope.count++;
-      
-  };
+    $scope.increment = function() {
+        if (incremented) {
+            return;
+        }
+        $scope.count++;
+
+    };
 });
-
